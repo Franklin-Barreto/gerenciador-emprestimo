44c61cb678ee0dfeec9fd92eba8c184943bed136
Franklin Barreto <franklin-barreto@hotmail.com>
2021-08-20 23:45:13 -0300

    título amigável de status

--- src/main/java/br/com/santander/gerenciadoremprestimo/model/Status.java
+++ src/main/java/br/com/santander/gerenciadoremprestimo/model/Status.java
@@ -2,5 +2,15 @@ package br.com.santander.gerenciadoremprestimo.model;
 
 public enum Status {
 
-	PENDENTE,NEGADA,APROVADA
+	PENDENTE("Pendente"), NEGADA("Negada"), APROVADA("Aprovada");
+
+	private String descricao;
+
+	private Status(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
 }
